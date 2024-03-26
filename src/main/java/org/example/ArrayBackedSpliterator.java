4b7a5353c057ea671a997d36e445d324728afd79
YichuanSun <Yichuan_Sun@163.com>
2024-03-26 12:53:24 +0800

    init

---
+++ src/main/java/org/example/ArrayBackedSpliterator.java
@@ -0,0 +1,111 @@
+/*
+ * The Alluxio Open Foundation licenses this work under the Apache License, version 2.0
+ * (the "License"). You may not use this work except in compliance with the License, which is
+ * available at www.apache.org/licenses/LICENSE-2.0
+ *
+ * This software is distributed on an "AS IS" basis, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied, as more fully set forth in the License.
+ *
+ * See the NOTICE file distributed with this work for information regarding copyright ownership.
+ */
+
+package org.example;
+
+import java.util.Comparator;
+import java.util.Spliterator;
+import java.util.Spliterators;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.function.Consumer;
+
+/**
+ * The code is mainly copied from ArraySpliterator in {@link Spliterators}.
+ * @param <T> the type
+ */
+public class ArrayBackedSpliterator<T> implements Spliterator<T> {
+  private final Object[] mArray;
+  private int mIndex;        // current index, modified on advance/split
+  private final int mFence;  // one past last index
+  private final int mCharacteristics;
+  private final int mEnd;    // the end position of current batch.
+  public static AtomicInteger mInt = new AtomicInteger(1);
+
+  /**
+   * @param array the array, assumed to be unmodified during use
+   * @param origin the least index (inclusive) to cover
+   * @param fence one past the greatest index to cover
+   * @param end the end position of current batch(exclusive)
+   * @param additionalCharacteristics Additional spliterator characteristics
+   * of this spliterator's source or elements beyond {@code SIZED} and
+   * {@code SUBSIZED} which are always reported
+   */
+  public ArrayBackedSpliterator(Object[] array, int origin, int fence, int end,
+                                int additionalCharacteristics) {
+    System.out.println("$$$$$$$ begin: " + origin + " end: " + fence);
+    mArray = array;
+    mIndex = origin;
+    mFence = fence;
+    mCharacteristics = additionalCharacteristics | Spliterator.SIZED | Spliterator.SUBSIZED;
+    mEnd = end;
+  }
+
+  @Override
+  public Spliterator<T> trySplit() {
+    System.out.println("Thread amount:" + Thread.activeCount());
+    if (mFence == mEnd) {
+      System.out.println("!!! " + mIndex + " " + mFence);
+    }
+    int lo = mIndex, mid = (lo + mFence) >>> 1;
+    System.out.println("element amount: " + (mid - lo));
+    if (lo >= mid) {
+      return null;
+    } else {
+      System.out.println("current spliterator number: " + mInt.addAndGet(1));
+      return new ArrayBackedSpliterator<>(mArray, lo, mIndex = mid, mEnd, mCharacteristics);
+    }
+  }
+
+  @SuppressWarnings("unchecked")
+  @Override
+  public void forEachRemaining(Consumer<? super T> action) {
+    Object[] a;
+    int i;
+    int hi; // hoist accesses and checks from loop
+    if (action == null) {
+      throw new NullPointerException();
+    }
+    if ((a = mArray).length >= (hi = mFence) && (i = mIndex) >= 0 && i < (mIndex = hi)) {
+      do {
+        action.accept((T) a[i]);
+      } while (++i < hi);
+    }
+  }
+
+  @Override
+  public boolean tryAdvance(Consumer<? super T> action) {
+    if (action == null) {
+      throw new NullPointerException();
+    }
+    if (mIndex >= 0 && mIndex < mFence) {
+      @SuppressWarnings("unchecked") T e = (T) mArray[mIndex++];
+      action.accept(e);
+      return true;
+    }
+    return false;
+  }
+
+  @Override
+  public long estimateSize() { return mFence - mIndex; }
+
+  @Override
+  public int characteristics() {
+    return mCharacteristics;
+  }
+
+  @Override
+  public Comparator<? super T> getComparator() {
+    if (hasCharacteristics(Spliterator.SORTED)) {
+      return null;
+    }
+    throw new IllegalStateException();
+  }
+}
---
+++ src/test/java/AppTest.java
@@ -0,0 +1,64 @@
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+import org.example.ArrayBackedSpliterator;
+
+import java.util.List;
+import java.util.Spliterator;
+import java.util.concurrent.ExecutionException;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest
+    extends TestCase
+{
+  /**
+   * Create the test case
+   *
+   * @param testName name of the test case
+   */
+  public AppTest(String testName )
+  {
+    super( testName );
+  }
+
+  /**
+   * @return the suite of tests being tested
+   */
+  public static Test suite()
+  {
+    return new TestSuite( AppTest.class );
+  }
+
+  /**
+   * Rigourous Test :-)
+   */
+  public void testApp()
+  {
+    assertTrue( true );
+  }
+
+  public void testArrayBackedSpliterator() {
+    int mSize = 1000;
+    Integer[] mArray = new Integer[mSize];
+    for (int i = 0; i < mSize; i++) {
+      mArray[i] = i;
+    }
+    for (int i = 0; i < mSize; i++) {
+      System.out.print(mArray[i] + " ");
+    }
+    System.out.println("\n");
+    ArrayBackedSpliterator
+        spliterator = new ArrayBackedSpliterator(mArray, 0, mSize, mSize, Spliterator.SIZED);
+    Stream<Integer> stream = StreamSupport.stream(spliterator, true);
+    List<Integer> li = stream.map(x -> x + 1).collect(Collectors.toList());
+    for (int i = 0; i < mSize; i++) {
+      System.out.print(li.get(i) + " ");
+    }
+    System.out.println("\n");
+  }
+}
